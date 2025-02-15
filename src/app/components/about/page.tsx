6cba4f19d3dc5ad16f7bb62c018e1a5606504508
Mubashir Khan <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2025-02-15 22:07:25 +0500

    Initial commit from Create Next App

---
+++ src/app/components/about/page.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Welcome to The Pawfect Store Where Every Pet Deserves the Best! 🐾✨</h1>  
+      <div>
+       At "The Pawfect Store", we believe that your furry, feathery, and scaly friends 
+       deserve nothing but the finest! From premium pet accessories to stylish essentials, 
+       we offer a carefully curated collection to keep your pets happy, healthy, and pampered. 
+       Whether you need cozy beds, fun toys, nutritious treats, or trendy collars, we have got 
+       it all under one roof. Our products are designed with love, ensuring comfort, durability, 
+       and style for your beloved companions. Because when it comes to your pets, only "pawfection" will do! 🐶🐱🐾
+       </div>  
+      <h1>Shop now and treat your pet to something special! 🛍️✨</h1>
+    </div>
+  )
+}
+
+export default page
